9583024cde6528bdd0dd3fa7176d2aff32ca6468
phil-y <pyuengling@verizon.net>
2015-07-17 16:04:46 -0400

    keep unbound alive

--- noitkeeper/noitkeeper.sh
+++ noitkeeper/noitkeeper.sh
@@ -11,3 +11,9 @@ jezebelrc=$?; if [[ $jezebelrc != 0 ]];
   then echo "Jezebel is not running";
     /etc/init.d/jezebel start;
 else echo "Jezebel is running"; fi
+
+pgrep unbound
+unboundrc=$?; if [[ $unboundrc != 0 ]];
+  then echo "unbound is not running";
+    /etc/init.d/circonus-unbound start;
+  else echo "unbound is running"; fi
